85cecd8ba5d426bbf2802ec1d7052b6997720fb4
ecalik <ecalik@gmail.com>
2015-01-19 09:36:57 +0100

    database configuration

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'test';
+$db['default']['hostname'] = 's179.webhostingserver.nl';
+$db['default']['username'] = 'deb69644_admin';
+$db['default']['password'] = 'allahuekber';
+$db['default']['database'] = 'deb69644_hourreg';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
